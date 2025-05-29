9f9d31613750971aa93385a8d25604042c2d0ce4
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-05-29 17:32:35 -0400

    fix decimal to hex function

--- src/index.js
+++ src/index.js
@@ -22,34 +22,25 @@ const binToDecimal = (value) => {
 }
 
 const decimalToHex = (value) => {
-    const hexTable = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","F"]
-    let divisionResult = value / 16
+    const hexTable = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D", "E","F"]
+    let divisionResult = value
     let hexNumber = []
-
-    if(Number.isInteger(divisionResult)){
-        console.log(divisionResult)
-        hexNumber.push(hexTable[divisionResult])
-    }
-
-    else {
-        hexNumber.push(hexTable[value % 16])
-    }
-
-    divisionResult = value
 
     do{
         const division = divisionResult / 16
+        const divisionResidue = divisionResult % 16
         divisionResult = !Number.isInteger(division)? Math.trunc(division) : division
-        if(divisionResult > 16) {
-            const divisionResidue = divisionResult % 16
-            hexNumber.push(hexTable[divisionResidue])
-        }
-        else {
-            hexNumber.push(hexTable[divisionResult])
+        hexNumber.push(hexTable[divisionResidue])
+        if(division < 16) {
+           hexNumber.push(hexTable[divisionResult])
         }
 
     } while(divisionResult > 16 )
     return hexNumber.reverse().join("")
 }
 
-console.log(decimalToHex(2742))
+const hexToDecimal = (value) => {
+    
+}
+
+console.log(decimalToHex(16))
