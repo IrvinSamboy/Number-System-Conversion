738b5e9a9ea3970bd7e2534e2270e37b652b0ea9
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-05-29 14:25:26 -0400

    fixing decimal to bin function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 
 const decimalToBin = (value) => {
     let divisionResult = value
-    let binValue = []
+    const initialBinValue = divisionResult % 2 === 0? 0 : 1
+    let binValue = [initialBinValue]
     do {
         const division = divisionResult/2
         divisionResult = !Number.isInteger(division)? Math.trunc(division) : division
         binValue.push(divisionResult % 2 === 0? 0: 1)
-    } while(divisionResult > 0)
+    } while(divisionResult > 1)
     return binValue.reverse().join("")
 }
 
 const binToDecimal = (value) => {
     let binValue = value.toString().split("").reverse()
     let decimalValue = 0
-    console.log(bin)
     for(i = 0; i<binValue.length; i++){
         decimalValue = decimalValue + (Number(binValue[i]) * Math.pow(2, i))
-        console.log(Number(binValue[i]) * Math.pow(2, i))
     }
 
     return decimalValue
 }
 
 console.log(decimalToBin(46))
-console.log(binToDecimal("010111"))
+console.log(binToDecimal(101110))
